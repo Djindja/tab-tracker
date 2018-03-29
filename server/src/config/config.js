1a07c4d7068ec67c42aefbabea51095adb48206c
Djindja <stefansdjindjic@gmail.com>
2018-03-29 22:09:37 +0200

    part3

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -9,5 +9,8 @@ module.exports = {
       host: process.env.HOST || 'localhost',
       strorage: './tabtracker.sqlite' 
     }
+  },
+  authentication: {
+    jwtSecret: process.env.JWT_SECRET || 'secret'
   }
 }
